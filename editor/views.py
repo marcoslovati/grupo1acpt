df06866481cc7d0223746971ba81f1229182dd4d
Ramon <Rahmon@users.noreply.github.com>
2016-06-28 00:13:17 -0300

    Tela de Login
    Tela de login com uma listagem de usuários

--- editor/views.py
+++ editor/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+from editor.models import Usuario
 
 # Create your views here.
+
+def index(request):
+    usuarios = Usuario.objects.all().order_by('nome')
+    context = {'all_usuarios' : usuarios}
+    return render(request, 'index.html', context)
