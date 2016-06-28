7c9b0aa9937e1f1e37c861f503fba01de116ef3c
Marcos Paulo <marcos_lovati@hotmail.com>
2016-06-28 08:58:13 -0300

    Início do desenvolvimento da tela inicial

---
+++ editor/urls.py
@@ -0,0 +1,6 @@
+﻿from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.inicial),
+]
--- editor/views.py
+++ editor/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Texto
 
 # Create your views here.
+
+def inicial(request):
+	textos_moderador = Texto.objects.filter(moderador=1).order_by('data_encontro')
+	textos_convidado = Texto.objects.filter(moderador=1).order_by('data_encontro')
+	return render(request, 'inicial.html', {'textos_moderador': textos_moderador})
