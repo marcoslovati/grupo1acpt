704aeee81c5765c9c3ff2a109c2de46ec9e43cee
Marcos Paulo <marcos_lovati@hotmail.com>
2016-06-30 21:39:28 -0300

    Correção da view inicial e da consulta de textos nos quais usuário é convidado

--- editor/views.py
+++ editor/views.py
@@ -6,9 +6,9 @@ from .models import Usuario
 # Create your views here.
 
 def inicial(request):
-	textos_moderador = Texto.objects.filter(moderador=1).order_by('data_encontro')
-	textos_convidado = Texto.objects.filter(moderador=1).order_by('data_encontro')
-	return render(request, 'inicial.html', {'textos_moderador': textos_moderador})
+	textos_moderador = Texto.objects.filter(moderador=1).order_by('data_encontro') # trocar depois '1' pelo id do usuario logado
+	textos_convidado = Texto.objects.filter(texto_usuario__usuario=1).exclude(moderador=1).order_by('data_encontro')
+	return render(request, 'inicial.html', {'textos_moderador': textos_moderador, 'textos_convidado': textos_convidado})
 
 def index(request):
     usuarios = Usuario.objects.all().order_by('nome')
